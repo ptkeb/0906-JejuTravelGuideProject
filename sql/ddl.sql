552bb6a407c738e61c3e0644df87e3e7662e0bf7
pt <ptkeb46@gmail.com>
2021-09-10 10:31:25 +0900

    Update ddl.sql

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -3,11 +3,12 @@ DROP TABLE sight cascade constraint;
 DROP TABLE category cascade constraint;
 
 CREATE TABLE sight (
-       sightid          			VARCHAR2(20) PRIMARY KEY,
-       sightname               		VARCHAR2(40) NOT NULL,
-       sightregion         			VARCHAR2(20) NOT NULL,
+       sightid          			VARCHAR2(80) PRIMARY KEY,
+       sightname               		VARCHAR2(80) NOT NULL,
+       sightregion         			VARCHAR2(80) NOT NULL,
        sightcategory                VARCHAR2(10) NOT NULL,
        imagepath       					VARCHAR2(1000)
+        
 );
 
 CREATE TABLE category (
